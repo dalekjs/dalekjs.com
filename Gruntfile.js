3d30f2a0349a3cfac9bbc03d9b46ca15fb80fe71
asciidisco <public@asciidisco.com>
2013-07-23 14:32:24 +0200

    Added template processing & grunt server/livereload functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,44 @@
 module.exports = function(grunt) {
   'use strict';
 
+  var fs = require('fs');
+
+  var loadAssets = function (Handlebars) {
+    var header = String(fs.readFileSync(__dirname + '/assets/header.html', 'utf-8'));
+    var footer = String(fs.readFileSync(__dirname + '/assets/footer.html', 'utf-8'));
+    // partials
+    Handlebars.registerPartial('assets/header.html', header);
+    Handlebars.registerPartial('assets/footer.html', footer);
+    return Handlebars;
+  };
+
+  var loadVars = function (contents) {
+    // load vars
+    var vars = {};
+
+    // defaults
+    var defaults = {
+      title: 'DalekJS - Automated cross browser testing with JavaScript',
+      desc: 'DalekJS - Exterminate all the bugs'
+    };
+
+    var subtrs = contents.substring(contents.lastIndexOf('data-templatevars=')).replace("data-templatevars='", '');
+    try {
+    vars = JSON.parse(subtrs.substr(0, subtrs.indexOf("'")));
+    } catch (e) {}
+
+    Object.keys(defaults).forEach(function (key) {
+      if (!vars[key]) {
+        vars[key] = defaults[key];
+      }
+    });
+
+    return vars;
+  };
+
   grunt.initConfig({
 
+    // live rendering & delivery of handlebars
     connect: {
       livereload: {
         options: {
@@ -10,8 +46,24 @@ module.exports = function(grunt) {
           middleware: function(connect, options) {
             return[
               require('connect-livereload')(),
-              connect.static(options.base),
-              connect.directory(options.base)
+              function (req, res) {
+
+                // check if we need to process the template data
+                if (req.url.substring(req.url.lastIndexOf('.')) === '.html') {
+                  var Handlebars = require('handlebars');
+                  // load file contents
+                  var contents = fs.readFileSync(__dirname + '/' + req.url, 'utf-8');
+                  loadAssets(Handlebars);
+                  var vars = loadVars(contents);
+                  // send compiled template contents
+                  res.end(Handlebars.compile(contents)(vars));
+                  return true;
+                }
+
+                // return all other resources static
+                return connect.static(options.base).apply(connect, arguments);
+              },
+              connect.directory(options.base),
             ];
           }
         }
@@ -20,7 +72,7 @@ module.exports = function(grunt) {
 
     // clean
     clean: {
-      build: ['build']
+      build: ['build', 'temp']
     },
 
     // JS min
@@ -108,7 +160,7 @@ module.exports = function(grunt) {
       img: {src: ['img/**/*.{png,jpg,jpeg,gif,svg}'], dest: 'build/img', expand: true, flatten: true, filter: 'isFile'},
       html: {src: ['index.html', '404.html'], dest: 'build/', expand: true, flatten: true, filter: 'isFile'},
       meta: {src: ['robots.txt', 'crossdomain.xml', 'favicon.ico', 'humans.txt', 'apple-touch*.png'], dest: 'build/', expand: true, flatten: true, filter: 'isFile'},
-      pages: {src: ['pages/*.html'], dest: 'build/pages', expand: true, flatten: true, filter: 'isFile'},
+      pages: {src: ['temp/*.html'], dest: 'build/pages', expand: true, flatten: true, filter: 'isFile'},
       docs: {src: ['docs/*.html'], dest: 'build/docs', expand: true, flatten: true, filter: 'isFile'}
     },
 
@@ -140,6 +192,30 @@ module.exports = function(grunt) {
 
   });
 
+  // register handlebars compile step
+  grunt.registerTask('templates', function () {
+    var Handlebars = require('handlebars');
+    var files = fs.readdirSync(__dirname + '/pages');
+
+    // check if the output directory exists
+    if (!fs.existsSync(__dirname + '/temp')) {
+      fs.mkdirSync(__dirname + '/temp');
+    }
+
+    files.forEach(function (file) {
+      if (file.substring(file.lastIndexOf('.')) === '.html') {
+        // load file contents
+        var contents = fs.readFileSync(__dirname + '/pages/' + file, 'utf-8');
+        loadAssets(Handlebars);
+        var vars = loadVars(contents);
+        // send compiled template contents
+        var compiled = Handlebars.compile(contents)(vars);
+        fs.writeFileSync(__dirname + '/temp/' + file, compiled);
+        grunt.log.ok('Generated file: temp/' + file);
+      }
+    });
+  });
+
   // load 3rd party tasks
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -155,6 +231,6 @@ module.exports = function(grunt) {
 
   // Default
   grunt.registerTask('default', ['connect', 'watch']);
-  grunt.registerTask('build', ['clean', 'sass', 'cssmin', 'uglify', 'copy', 'rev', 'usemin', 'htmlmin', 'compress']);
+  grunt.registerTask('build', ['clean', 'sass', 'cssmin', 'uglify', 'templates', 'copy', 'rev', 'usemin', 'htmlmin', 'compress']);
 
 };
